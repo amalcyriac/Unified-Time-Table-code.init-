3a40eb43ad89c9b6313aed8a741a173e5f87f656
Dhanuanth <66349240+Dhanuanth@users.noreply.github.com>
2021-09-25 20:45:43 +0530

    Add files via upload

---
+++ UI/student/home_student.js
@@ -0,0 +1,23 @@
+var tt = {
+    "0":{
+        "1":"Physics",
+        "2":"sadf",
+        "3":"Free"
+    },
+    "1":{
+        "1":"TIC",
+    }
+}
+
+var myTab = document.getElementById("timeTable");
+
+for (i = 0; i < myTab.rows.length; i++) {
+
+    var objCells = myTab.rows.item(i).cells;
+
+    for (var j = 1; j < objCells.length; j++) {
+        
+         objCells.item(j).children[0].innerHTML=tt[i][j];
+    }
+  
+}
